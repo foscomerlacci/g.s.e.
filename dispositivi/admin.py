b594dbf53ec175928c6ca4eb7306e98bb97ea6e5
foscomerlacci <foscomerlacci@tiscali.it>
2019-10-15 18:35:34 +0200

    versione 0.1.0 - primo commit

--- dispositivi/admin.py
+++ dispositivi/admin.py
@@ -249,7 +249,7 @@ from django.contrib.auth.admin import UserAdmin
 
 class MyUserAdmin(UserAdmin):
     list_display = ['first_name', 'last_name', 'is_staff', 'is_active', 'last_login']
-
+    ordering = ['is_active', 'last_name']
 admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)
 
