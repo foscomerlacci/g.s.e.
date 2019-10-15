159d490b729169c7bdd70a2021b5b2fc3d09028d
foscomerlacci <foscomerlacci@tiscali.it>
2019-10-15 18:38:29 +0200

    versione 0.1.1 - aggiunto campo is_active in visualizzazione Users admin list display

--- dispositivi/admin.py
+++ dispositivi/admin.py
@@ -248,8 +248,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
 class MyUserAdmin(UserAdmin):
+
     list_display = ['first_name', 'last_name', 'is_staff', 'is_active', 'last_login']
     ordering = ['is_active', 'last_name']
+
 admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)
 
